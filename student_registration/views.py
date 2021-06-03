15746132d469badfa88675cc65310e0375fab4ae
Ashish Maharjan <amaharjan1032@gmail.com>
2021-06-03 14:14:23 +0545

    urls done

--- student_registration/views.py
+++ student_registration/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def student_list(request):
+    return
+
+
+def student_form(request):
+    return
+
+
+def student_delete(request):
+    return
