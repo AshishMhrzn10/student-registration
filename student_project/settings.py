94534aeef59c46d67d073f969e708f5d1f3ea69e
Ashish Maharjan <amaharjan1032@gmail.com>
2021-06-03 14:04:20 +0545

    models and postgres setup

--- student_project/settings.py
+++ student_project/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'student_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'StudentDB',
+        'USER': 'postgres',
+        'PASSWORD': 'password12345',
+        'HOST': 'localhost'
     }
 }
 
--- student_registration/models.py
+++ student_registration/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Faculty(models.Model):
+    fname = models.CharField(max_length=50)
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    std_code = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    phone = models.IntegerField()
+    faculty = models.ForeignKey(Faculty, on_delete = models.CASCADE)
