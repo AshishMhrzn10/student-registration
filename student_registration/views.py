4bf2e8efa809c676d62f61dd5f469721de2976aa
Ashish Maharjan <amaharjan1032@gmail.com>
2021-06-03 14:21:45 +0545

    templates setup done

--- student_registration/views.py
+++ student_registration/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def student_list(request):
-    return
+    return render(request, "student_registration/student_list.html")
 
 
 def student_form(request):
-    return
+    return render(request, "student_registration/student_form.html")
 
 
 def student_delete(request):
