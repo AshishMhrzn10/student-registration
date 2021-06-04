a5b5e3ba687ad774c146fb586e42a9a1bf2936c4
Ashish Maharjan <amaharjan1032@gmail.com>
2021-06-04 11:09:49 +0545

    Update  done

--- student_registration/urls.py
+++ student_registration/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.student_form),
-    path('list/', views.student_list),
+    path('', views.student_form, name='student_insert'),
+    path('<int:id>/', views.student_form, name='student_update'),
+    path('list/', views.student_list, name='student_list'),
 ]
--- student_registration/views.py
+++ student_registration/views.py
@@ -8,12 +8,21 @@ def student_list(request):
     return render(request, "student_registration/student_list.html", context)
 
 
-def student_form(request):
+def student_form(request, id=0):
     if request.method == "GET":
-        form = StudentForm()
+        if id==0:       #Insert operation
+            form = StudentForm()
+        else:       #Update operation
+            student = Student.objects.get(pk=id)
+            form = StudentForm(instance=student)
         return render(request, "student_registration/student_form.html",{'form':form})
     else:
-        form = StudentForm(request.POST)
+        if id == 0:     #Insert operation
+            form = StudentForm(request.POST)
+        else:           #Update operation
+            student = Student.objects.get(pk=id)
+            form = StudentForm(request.POST, instance=student)
+
         if form.is_valid():
             form.save()
         return redirect('/student/list')
